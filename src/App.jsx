27ffb8b412435e0db8fa8acb1389683608e3de7b
qasimgit <qasimpkkhi@gmail.com>
2020-08-15 12:04:12 +0500

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import {} from "@material-ui/core";
+
+import {Home} from './components/home'
 
 const App = () => {
-    return (
-        <div>
-            Qasim
-        </div>
-    )
-}
+  return (
+    <Router>
+      <div className="navbar">
+        <h1>Shoe Store</h1>
+        <nav className="links">
+          <Link to="/">Home</Link>
+          <Link to="/products">Products</Link>
+          <Link to="/contactus">Contact</Link>
+        </nav>
+        <div></div>
+      </div>
+
+      <Routes>
+          <Route path="/" element={<Home/>} />
+      </Routes>
+    </Router>
+  );
+};
 export default App;
---
+++ src/components/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <div className='home'>
+        <div className='home-cont'>
+
+      <h1>Wellcome to the Store</h1>
+      <h2>Enjoy the deals</h2>
+      <h2>Save upto 50%OFF</h2>
+        </div>
+    </div>
+  );
+};
